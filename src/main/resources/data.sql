b7250c5f297e46f0d8f727bda9a7a469bf9e34b7
Zining Wang <wangzining0412@gmail.com>
2023-06-03 23:01:27 -0500

    delete script that no longer works

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,3 @@ insert into products(name, category, price, inventory) values ('product 23', 'to
 
 
 -- insert some fake data to shopping cart
-insert into carts(id, total) values (000000, 145.7);
-insert into carts(id, total) values (111111, 27);
-insert into carts(id, total) values (222222, 657.99);
