e4e345541dba427d54396e254699b25a5d3294f8
Wang306 <60589080+Wang306@users.noreply.github.com>
2023-06-04 22:35:58 -0500

    Update package-info.java

--- src/main/java/com/depaul/depaulmarketplace/shoppingcart/package-info.java
+++ src/main/java/com/depaul/depaulmarketplace/shoppingcart/package-info.java
@@ -2,7 +2,20 @@ package com.depaul.depaulmarketplace.shoppingcart;
 
 /**
  * ShoppingCart Feature by Zining Wang 
- * Setup basic shopping cart table
  * 
+ * tests have loading error 
+ * but still write some for each service feature in a possible work way
  * 
+ * Documentations on features that were not covered in class :
+ * 
+ * additional finders in ShoppingCartRepository
+ * 
+ * mappedBy attribute, to specify the CartItems are owned by ShoppingCart
+ * 
+ * cascade attribute
+ * indicates all operations used on the ShoppingCart entiry will be cascaded to CartItem Entity
+ * 
+ * orphanRemoval = true
+ * indicates if a cart item is deleted in a shopping cart, 
+ * this cart item will also be deleted in cart item database
  */
